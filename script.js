d10811c5334bdc0303a36d9ec59e4d407873bce4
Bartosz Tkacz <76884859+b-tkacz@users.noreply.github.com>
2021-08-12 11:41:53 +0200

    new script file

--- script.js
+++ script.js
@@ -21,52 +21,20 @@ window.onclick = function(event) {
 }
 
 
-// ON CLICK 
+// DROPDOWN 
 
-// document.getElementById('dropBtn').onclick = function(){
-//   document.getElementById("dropDownContent").style.display = "block";
-// }
+// let dropBtn = document.getElementById("dropBtn")
+// let dropDownContent = document.getElementById("dropDownContent")
 
-// document.getElementById('dropBtn').onclick = function () {
-//   let menu = document.getElementById("dropDownContent");
-//   if (menu.style.display = "none") {
-//     menu.style.display ="block";
-//   }
-//   else {
-//     menu.style.display === "none";
-//   }
-// }
-
-
-
-
-
-// function dropdown () {
-//   if ()
+// dropBtn.onclick = function() {
+//    if (dropDownContent.style.display = "none") {
+//      dropDownContent.style.display = "block";
+//     }
+//     else if(dropDownContent.style.display = "block") {
+//       onclick.dropDownContent.style.display = "none"
+//     }
 // }
 
 
-// BLURRED BACKGROUND
 
-const loadText = document.querySelector('.loading-text')
-const bg  =  document.querySelector('.bg')
 
-let load = 0 
-
-let int = setInterval(blurring, 30)
-
-function blurring() {
-    load++
-
-    if (load > 99) {
-      clearInterval(int)
-    }
-
-    loadText.innerText = `${load}%`
-    loadText.style.opacity = scale(load, 0, 100, 1, 0)
-    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
-}
-
-function scale (number, inMin, inMax, outMin, outMax) {
-  return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
-}
