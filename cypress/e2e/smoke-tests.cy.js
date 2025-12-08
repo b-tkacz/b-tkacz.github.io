e1223c07f39b5bcedd636ae6c42f85ed3c444e4a
Bartosz T <astmatyk@gmail.com>
2025-12-08 11:58:03 +0100

    consolidate tests into one spec file

--- cypress/e2e/smoke-tests.cy.js
+++ cypress/e2e/smoke-tests.cy.js
@@ -22,4 +22,16 @@ describe("Homepage Smoke", () => {
       };
     });
   });
+
+  it("nav links update hash and focus target", () => {
+    cy.get(".nav-container--box a").contains("Projects").click();
+    cy.location("hash").should("eq", "#projects");
+    // check the target element exists and is visible
+    cy.get("#projects").should("be.visible");
+  });
+
+  it("Contact control opens modal (if implemented as button/link)", () => {
+    cy.get("#openModal").click();
+    cy.get("#modal").should("be.visible");
+  });
 });
