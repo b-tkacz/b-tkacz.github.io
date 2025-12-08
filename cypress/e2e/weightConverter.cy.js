967f35f234de9246b2e8ac4cafc1ad8e8cfb75bf
Bartosz T <astmatyk@gmail.com>
2025-12-08 19:28:58 +0100

    create test for weight converter, rename page typos and update files with correct spelling

---
+++ cypress/e2e/weightConverter.cy.js
@@ -0,0 +1,11 @@
+describe("Weight Converter Page", () => {
+  beforeEach(() => {
+    cy.visit("/weight-converter.html");
+    cy.viewport(1280, 720);
+  });
+
+  it("Check title and placeholder value", () => {
+    cy.get("h1.converter-title").should("have.text", "Weight Converter");
+    cy.shouldHavePlaceholder("input#lbsInput", "Enter Pounds...");
+  });
+});
