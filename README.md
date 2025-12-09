accc1aa8cf911392f6a7d68f1873af6580a27264
Bartosz T <astmatyk@gmail.com>
2025-12-09 11:21:12 +0100

    create npm scripts and update config files

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # b-tkacz.github.io
 
-<!-- Welcome to my portfolio. I'm constantly developing this project so feel free to leave some feedback and come back again!   -->
+Welcome to my portfolio. I'm constantly developing this project so feel free to leave some feedback and come back again!
---
+++ cypress.dev.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://127.0.0.1:5500",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/results",
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+});
